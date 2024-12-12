b4afa8f040493ab1f7bd9ef2e9724c465b730c67
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-12-12 18:22:35 +0900

    feat: add complete answer of day09

--- src/day09.ts
+++ src/day09.ts
@@ -1,44 +1,81 @@
+import { reduceAsyncIterator } from "./util.ts";
+
 const inputStream = Deno.stdin.readable
   .pipeThrough(new TextDecoderStream());
+const input = await reduceAsyncIterator(inputStream, (sum, v) => sum + v, "");
 
-let input = "";
-for await (const chunk of inputStream) {
-  input += chunk;
-}
+type File = { id: number | null; size: number };
 
-type Disk = (number | null)[];
-
-let disk: Disk = [];
-let isBlank = false;
-let fileId = 0;
-for (const v of input.trim()) {
-  if (isBlank) {
-    disk = disk.concat(Array.from({ length: parseInt(v) }, () => null));
+const disk: File[] = [...input.trim()].map((v, i) => {
+  if (i % 2 === 0) {
+    return { id: i / 2, size: parseInt(v) };
   } else {
-    disk = disk.concat(Array.from({ length: parseInt(v) }, () => fileId));
-    fileId += 1;
+    return { id: null, size: parseInt(v) };
   }
-  isBlank = !isBlank;
-}
+});
 
-const compactDisk = function (disk: Disk): Disk {
-  const newDisk = disk.concat();
-
-  for (let i = disk.length - 1; i >= 0; i--) {
-    if (newDisk[i] === null) continue;
-
-    const firstBlank = newDisk.indexOf(null);
-    if (firstBlank >= i) break;
-    newDisk[firstBlank] = newDisk[i];
-    newDisk[i] = null;
-  }
-
-  return newDisk;
+const diskContents = function (disk: File[]): (number | null)[] {
+  return disk.flatMap((f) => Array.from({ length: f.size }, () => f.id));
 };
 
-const calcChecksum = function (disk: Disk): number {
-  return disk.reduce<number>((sum, v, i) => sum + (v ?? 0) * i, 0);
+const diskCheckSum = function (disk: File[]): number {
+  return diskContents(disk).reduce<number>(
+    (sum, v, i) => sum + (v ?? 0) * i,
+    0,
+  );
 };
 
-const compactedDisk = compactDisk(disk);
-console.log(calcChecksum(compactedDisk));
+const collapseDiskContents = function (disk: File[]): void {
+  while (true) {
+    let updated = false;
+    for (let i = 0; i < disk.length; i++) {
+      if (disk[i].size === 0) {
+        disk.splice(i, 1);
+        updated = true;
+        break;
+      }
+      if (disk[i].id === disk[i + 1]?.id) {
+        disk.splice(i, 2, {
+          id: disk[i].id,
+          size: disk[i].size + disk[i + 1].size,
+        });
+        updated = true;
+        break;
+      }
+    }
+
+    if (!updated) break;
+  }
+};
+
+const compactDisk = function (disk: File[]): void {
+  const maxId = disk.findLast((f) => f.id !== null)?.id ?? -1;
+  for (let id = maxId; id >= 0; id--) {
+    collapseDiskContents(disk);
+
+    const fileIndex = disk.findIndex((f) => f.id === id);
+    const file = disk[fileIndex];
+
+    const blankIndex = disk.findIndex((v) =>
+      v.id === null && v.size >= file.size
+    );
+    if (blankIndex === -1 || blankIndex >= fileIndex) continue;
+    const blank = disk[blankIndex];
+
+    disk.splice(
+      fileIndex,
+      1,
+      { id: null, size: file.size },
+    );
+    disk.splice(
+      blankIndex,
+      1,
+      { ...file },
+      { id: null, size: blank.size - file.size },
+    );
+  }
+};
+
+compactDisk(disk);
+// console.log(diskContents(disk).map((v) => v === null ? "." : `${v}`).join(""));
+console.log("Disk checksum:", diskCheckSum(disk));
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,21 @@
-export async function asyncIteratorToArray<T>(iter: AsyncIterable<T>) {
+export async function asyncIteratorToArray<T>(
+  iter: AsyncIterable<T>,
+): Promise<T[]> {
   const result = [];
   for await (const value of iter) {
     result.push(value);
+  }
+  return result;
+}
+
+export async function reduceAsyncIterator<T, U>(
+  iter: AsyncIterable<T>,
+  reducer: (acc: U, cur: T) => U,
+  initialValue: U,
+): Promise<U> {
+  let result = initialValue;
+  for await (const value of iter) {
+    result = reducer(result, value);
   }
   return result;
 }
